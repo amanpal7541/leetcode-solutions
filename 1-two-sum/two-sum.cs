502107ba89e1f4d23f873ac60846ca4ea08bebf8
amanpal7541 <amanpal7541@gmail.com>
2026-08-16 18:01:28 +0530

    Time: 4 ms (87.79%) | Memory: 49.8 MB (42.73%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int required = target - nums[i];
+
+            if (map.ContainsKey(required))
+            {
+                return new int[] { map[required], i };
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return new int[] { };
+    }
+}
