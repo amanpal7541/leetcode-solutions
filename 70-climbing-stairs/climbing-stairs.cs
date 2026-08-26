c33eeccaebd3726e81838f0a5d7d8d95f69e52de
amanpal7541 <amanpal7541@gmail.com>
2026-08-26 22:07:55 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (69.46%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n<=2) return n;
+        int a = 1;
+        int b = 2;
+        for(int i =3;i<=n;i++){
+            int c = a +b;
+            a = b;
+            b= c;
+
+        }
+        return b;
+    }
+}
